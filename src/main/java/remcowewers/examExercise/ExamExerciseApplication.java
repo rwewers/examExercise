7b8aa191cf54fdd5826481d2fe770354c6052c59
rwewers <remco@wewers.nl>
2021-02-26 08:57:35 +0100

    Springboot project started

---
+++ src/main/java/remcowewers/examExercise/ExamExerciseApplication.java
@@ -0,0 +1,13 @@
+package remcowewers.examExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/remcowewers/examExercise/ExamExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package remcowewers.examExercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
