0c2a3932c64e0d610b6ce240c8c4d75d54880e5b
rwewers <remco@wewers.nl>
2021-05-15 17:33:48 +0200

    little changes

--- src/main/java/remcowewers/examExercise/controller/AdminController.java
+++ src/main/java/remcowewers/examExercise/controller/AdminController.java
@@ -1,6 +1,9 @@
 package remcowewers.examExercise.controller;
 
+import remcowewers.examExercise.domain.User;
+import remcowewers.examExercise.service.UserService2;
 import remcowewers.examExercise.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -9,16 +12,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.List;
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
 
     private UserService userService;
+    private UserService2 userService2;
 
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+    @Autowired
+    public void setUserService(UserService2 userService2) {
+        this.userService2 = userService2;
     }
 
     @GetMapping("/all")
@@ -29,4 +40,13 @@ public class AdminController {
 
 
 
+    @GetMapping("/test")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> getAllUsers() throws IOException {
+        List<User> users = userService2.getAllUsers();
+        return ResponseEntity.ok(users);
+
+
+
+    }
 }
